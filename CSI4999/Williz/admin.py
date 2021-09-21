9357c93e1272182b192a96d62e774f55c1b50e8f
PoiOU1 <80789667+PoiOU1@users.noreply.github.com>
2021-09-20 20:20:19 -0400

    Added MortgageCo table for future functionality. Added license expiration date. Added MortgageCo to admin.py

--- CSI4999/Williz/admin.py
+++ CSI4999/Williz/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Appraiser, Lender, Realtor, RequestReset, Validation
+from .models import User, Appraiser, Lender, Realtor, RequestReset, Validation, MortgageCo
 
 # Register your models here.
 admin.site.register(User)
@@ -8,4 +8,4 @@ admin.site.register(Lender)
 admin.site.register(Realtor)
 admin.site.register(RequestReset)
 admin.site.register(Validation)
-
+admin.site.register(MortgageCo)
