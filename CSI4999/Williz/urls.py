d9cf28ac4273f8fb4c21ffe8574dce98439230d5
mkmoore3 <mmoore3@oakland.edu>
2021-10-06 21:39:54 -0400

    Update urls.py
    Williz root URL points to login page.

--- CSI4999/Williz/urls.py
+++ CSI4999/Williz/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.login, name="login"),
     path("login/", views.login, name="login"),
     path("register/", views.register, name="register"),
     path("profile/email/<email>", views.profile, name="profile"),
