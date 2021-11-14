0accd588662233a788b7da87c00aa05cc4dfe0ba
mkmoore3 <mmoore3@oakland.edu>
2021-11-14 16:42:31 -0500

    Bugfix: User update redirect
    User profile update redirect was broken when the root server URL was changed to direct to Williz/urls.py. Redirect fixed to redirect users to the search listings page.

--- CSI4999/Williz/views.py
+++ CSI4999/Williz/views.py
@@ -1293,7 +1293,7 @@ def edit_user_info(request):
             lender.mortgage_co = request.POST['CompanyInput']
         user.save()
         lender.save()
-    return HttpResponseRedirect("Williz/login")
+    return HttpResponseRedirect("/searchListings")
 
 
 def change_verification(request, email):
